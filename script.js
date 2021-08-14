f0f108740570906f0445ca1e619f28e444cc8a5a
Kirill Lavrov <lkv@myttk.ru>
2021-08-14 20:30:57 +0700

    feat (script.js): Added asynchronous array iteration function

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const eacher = function(arr, callback) {
+  let count = 0;
+  const timer = setInterval(function() {
+    callback(arr[count++]);
+    if (count >= arr.length) {
+      clearInterval(timer);
+    }
+  }, 0);
+},
+arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+eacher(arr, function(item){
+  console.log(item);
+});
